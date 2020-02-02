365964ddd7e54d4b4b33fe803a93cf1956650103
Pratik <pr.tikgohil@gmail.com>
2020-02-02 22:22:37 +0530

    final commit

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -9,4 +9,21 @@ def bubble_sort arr
  end
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+def bubble_sort_by(arr)
+ raise 'No block_given' unless block_given?
+ if yield(arr)
+  arr.each do
+   arr.each_with_index do |i, index|
+    next if index == arr.length - 1
+    if arr[index] < arr[index + 1]
+     arr[index], arr[index + 1] = arr[index + 1], arr[index] 
+    end
+   end
+  end
+ end
+ arr
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+ left.length - right.length
+end
